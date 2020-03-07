accf7d5dd0ba5e824cbe3a08e12e8d5759b96eac
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-03-06 16:30:21 -0800

    Add Sidenav

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import {  MatToolbarModule } from '@angular/material/toolbar';
-import {  MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
@@ -12,13 +14,17 @@ import {  MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }
